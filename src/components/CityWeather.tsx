8cf2e4691dc143576a537d94c894bbb45d2dfa04
Jozef Zeglen <zeglenjozef@gmail.com>
2023-06-30 11:18:16 +0200

    zadanie

--- src/components/CityWeather.tsx
+++ src/components/CityWeather.tsx
@@ -1,10 +1,29 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent } from 'react';
 import { toDate } from './tools';
 import { CityWeatherProps } from '../types/CityWeather';
 
 export const CityWeather: FunctionComponent<CityWeatherProps> = ({ name, country, description, icon, windSpeed, temp }) => (
-  <div>
-    CityWeather
+<div>
+    <div className="city-name">
+      <h2>
+        {name}, <span>{country}</span>
+      </h2>
+    </div>
+    <div className="date">
+            <span>{toDate()}</span>
+    </div>
+    <div className="icon-temp">
+      <img
+        className=""
+        src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+        alt={description}
+      />
+      {temp}
+      <sup className="deg">&deg;C</sup>
+    </div>
+    <div className="des-wind">
+      <p>{description}</p>
+      <p>Wind Speed: {windSpeed}m/s</p>
+    </div>
   </div>
-)
-
+);
--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+
 import { useState } from 'react';
 import axios from 'axios';
 import { InfinitySpin } from 'react-loader-spinner';
@@ -6,44 +6,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFrown } from '@fortawesome/free-solid-svg-icons';
 import { WeatherResponse } from '../types/WeatherResponse';
 import { CityWeather } from './CityWeather';
-import { toDate } from './tools';
 
 function Weather() {
   const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState<{ loading: Boolean, data?: WeatherResponse, error: Boolean }>({
+  const [weather, setWeather] = useState<{ loading: Boolean, data?: WeatherResponse[], error: Boolean }>({
     loading: false,
     data: undefined,
     error: false,
   });
-
-
 
   const search = (event: any) => {
     if (event.key === 'Enter') {
       event.preventDefault();
       setQuery('');
       setWeather({ ...weather, loading: true });
-      const url = 'https://api.openweathermap.org/data/2.5/weather';
-      const appid = 'f00c38e0279b7bc85480c3fe775d518c';
-      //console.log('Enter');
-      const params = {
-        q: query,
-        units: 'metric',
-        appid: appid
-      }
 
-      axios.get(url, { params })
-        .then((res) => {
-          console.log('res', res);
-          setWeather({ data: res.data, loading: false, error: false });
-        }).catch((error) => {
+      //Rozdelenie inputu ciarkou pre 2 mesta
+      const cities = query.split(',').map((city) => city.trim());
+
+      const promises = cities.map((city) => {
+        const url = 'https://api.openweathermap.org/data/2.5/weather';
+        const appid = 'f00c38e0279b7bc85480c3fe775d518c';
+        const params = {
+          q: city,
+          units: 'metric',
+          appid: appid,
+        };
+
+        return axios.get(url, { params })
+     });
+     Promise.all(promises)
+        .then((responses) => {
+          const weatherData = responses.map((res) => res.data);
+          setWeather({ data: weatherData, loading: false, error: false });
+        })
+        .catch((error) => {
           setWeather({ ...weather, data: undefined, error: true });
           setQuery('');
           console.log('error', error);
-
         });
-    }
-  };
+ }
+};
 
   return (
     <div>
@@ -80,30 +83,18 @@ function Weather() {
         </>
       )}
 
-      {weather && weather.data && weather.data.main && (
-        <div>
-          <div className="city-name">
-            <h2>
-              {weather.data.name}, <span>{weather.data.sys.country}</span>
-            </h2>
-          </div>
-          <div className="date">
-            <span>{toDate()}</span>
-          </div>
-          <div className="icon-temp">
-            <img
-              className=""
-              src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
-              alt={weather.data.weather[0].description}
-            />
-            {Math.round(weather.data.main.temp)}
-            <sup className="deg">&deg;C</sup>
-          </div>
-          <div className="des-wind">
-            <p>{weather.data.weather[0].description.toUpperCase()}</p>
-            <p>Wind Speed: {weather.data.wind.speed}m/s</p>
-          </div>
-        </div>
+{weather && weather.data && weather.data.length > 0 && (
+        weather.data.map((cityData, index) => (
+          <CityWeather
+            key={index}
+            name={cityData.name}
+            country={cityData.sys.country}
+            description={cityData.weather[0].description.toUpperCase()}
+            icon={cityData.weather[0].icon}
+            windSpeed={cityData.wind.speed}
+            temp={Math.round(cityData.main.temp)}
+          />
+        ))
       )}
     </div>
   );
